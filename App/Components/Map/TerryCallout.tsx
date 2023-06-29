0afffa31e3dc7772501d3890b862a5d8063952d3
ndbac <ryannguyen0303@gmail.com>
2023-06-29 23:32:09 +0700

    feat: setup terry map using react-native-map

---
+++ App/Components/Map/TerryCallout.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Callout} from 'react-native-maps';
+import isEmpty from 'lodash/isEmpty';
+import {
+  responsiveByHeight as rh,
+  responsiveByWidth as rw,
+} from '../../helpers/common';
+
+interface TerryCalloutProps {
+  description?: string;
+  title?: string;
+  imageUrls?: string[];
+}
+
+const TerryCallout = (props: TerryCalloutProps) => {
+  return (
+    <Callout>
+      <View>
+        <Text>{props.title}</Text>
+        <Text>{props.description}</Text>
+        {props.imageUrls && !isEmpty(props.imageUrls) && (
+          <Image source={{uri: props.imageUrls[0]}} />
+        )}
+        <TouchableOpacity style={styles.viewBtn}>
+          <Text>View</Text>
+        </TouchableOpacity>
+      </View>
+    </Callout>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewBtn: {
+    height: rh(48),
+    borderColor: '#BDBDBD',
+    borderWidth: 1,
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: rw(12),
+    marginTop: rh(4),
+  },
+});
+
+export default TerryCallout;
---
+++ App/Enums/map.ts
@@ -0,0 +1,11 @@
+export enum EMapType {
+  STANDARD = 'standard',
+  SATELLITE = 'satellite',
+  HYBRID = 'hybrid',
+  TERRAIN = 'terrain',
+}
+
+export enum EMapStyle {
+  DARK = 'dark',
+  LIGHT = 'light',
+}
