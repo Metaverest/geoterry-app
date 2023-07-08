9c6157474ef5629891240409989292a508101f85
ndbac <ryannguyen0303@gmail.com>
2023-07-08 18:30:36 +0700

    fix: empty google map on android

--- App/helpers/common.ts
+++ App/helpers/common.ts
@@ -1,5 +1,6 @@
 import { widthPercentageToDP, heightPercentageToDP } from 'react-native-responsive-screen';
 import { DEFAULT_REFERENCE_SCREEN_HEIGHT, DEFAULT_REFERENCE_SCREEN_WIDTH } from '../constants/common';
+import { Platform } from 'react-native';
 
 export const responsiveByWidth = (value: number, referenceScreenWidth: number = DEFAULT_REFERENCE_SCREEN_WIDTH) => {
   return widthPercentageToDP((value / referenceScreenWidth) * 100);
@@ -7,4 +8,8 @@ export const responsiveByWidth = (value: number, referenceScreenWidth: number = 
 
 export const responsiveByHeight = (value: number, referenceScreenHeight: number = DEFAULT_REFERENCE_SCREEN_HEIGHT) => {
   return heightPercentageToDP((value / referenceScreenHeight) * 100);
+};
+
+export const isIOSDevice = () => {
+  return Platform.OS === 'ios';
 };
