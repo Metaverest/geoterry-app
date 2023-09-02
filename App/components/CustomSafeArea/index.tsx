e3310653c0ced185189b2a759fe793081d48f302
trungphamduc0401 <trungphamduc0401@gmail.com>
2023-09-02 10:11:50 +0700

    fix: add background color

--- App/components/CustomSafeArea/index.tsx
+++ App/components/CustomSafeArea/index.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/react-in-jsx-scope */
+import { EColor } from 'App/enums/color';
 import { View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const CustomSafeArea = (props: any) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View
-      style={{
-        marginTop: insets.top,
-        marginBottom: insets.bottom,
-        flex: 1,
-        ...props.style,
-      }}>
-      {props.children}
+    <View style={{ width: wp('100'), height: hp('100'), backgroundColor: EColor.color_171717 }}>
+      <View
+        style={{
+          marginTop: insets.top,
+          marginBottom: insets.bottom,
+          flex: 1,
+          ...props.style,
+        }}>
+        {props.children}
+      </View>
     </View>
   );
 };
