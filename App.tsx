f6b144ef42e66c78c4f60c5546d9b05d1d6a8fbb
ndbac <ryannguyen0303@gmail.com>
2023-10-19 19:53:49 +0800

    chore: remove cmt

--- App.tsx
+++ App.tsx
@@ -6,16 +6,11 @@ import { startNetworkLogging } from 'react-native-network-logger';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import Navigation, { navigationRef } from './App/navigation';
-// import { removePropertyInDevice } from 'App/utils/storage/storage';
-// import { EDataStorageKey } from 'App/enums';
 
 setupI18N();
 startNetworkLogging();
 
 const App = () => {
-  // removePropertyInDevice(EDataStorageKey.ACCESS_TOKEN);
-  // removePropertyInDevice(EDataStorageKey.REFRESH_TOKEN);
-
   return (
     <SafeAreaProvider>
       <Provider store={store}>
