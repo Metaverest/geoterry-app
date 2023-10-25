aa6d292617415888d4ffa7db8a905d599957986a
Bac Nguyen <66194265+ndbac@users.noreply.github.com>
2023-10-26 00:06:51 +0800

    fix: use coordinate to address retrigger multiple time (#49)
    * fix: userCoordinateToAddress hook be triggered multiple time
    
    * fix: userCoordinateToAddress hook be triggered multiple time

--- App/hooks/useCoordinateToAddress.ts
+++ App/hooks/useCoordinateToAddress.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import MapView, { Address } from 'react-native-maps';
 
 interface LatLng {
@@ -6,28 +6,35 @@ interface LatLng {
   longitude: number;
 }
 
-const useCoordinateToAddress = (mapRef: React.RefObject<MapView>, location: LatLng, forceFetch?: boolean) => {
-  const [address, setAddress] = useState<Address | null>(null);
+const useCoordinateToAddress = (mapRef: React.RefObject<MapView>, location: LatLng, forceOverrideValue?: boolean) => {
+  const [address, setAddress] = useState<Address | null>();
+  const prevLocationRef = useRef<LatLng | null>(null);
 
   useEffect(() => {
-    (async () => {
-      if (location.latitude && location.longitude && mapRef.current) {
-        try {
-          const resAddress = await mapRef.current.addressForCoordinate({
-            latitude: location.latitude,
-            longitude: location.longitude,
-          });
-          setAddress(resAddress);
-        } catch (error) {
-          if (!forceFetch) {
-            setAddress(null);
+    if (
+      location.latitude !== prevLocationRef.current?.latitude ||
+      location.longitude !== prevLocationRef.current?.longitude
+    ) {
+      (async () => {
+        if (location.latitude && location.longitude && mapRef.current) {
+          try {
+            const resAddress = await mapRef.current.addressForCoordinate({
+              latitude: location.latitude,
+              longitude: location.longitude,
+            });
+            setAddress(resAddress);
+            prevLocationRef.current = location;
+          } catch (error) {
+            if (forceOverrideValue) {
+              setAddress(null);
+            }
+            console.log(error);
           }
-          console.log(error);
         }
-      }
-    })();
+      })();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mapRef, location]);
+  }, [location, mapRef]);
 
   return address;
 };
