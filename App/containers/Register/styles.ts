67e06128d600f98bf1c11100217b131386d0acb5
ndbac <ryannguyen0303@gmail.com>
2024-04-29 10:35:48 +0800

    chore: update login with social media component

--- App/containers/Register/styles.ts
+++ App/containers/Register/styles.ts
@@ -11,7 +11,7 @@ export const styles = StyleSheet.create({
   },
   mainContainer: { flex: 1 },
   image: {
-    marginTop: -rh(171),
+    marginTop: -rh(230),
     alignSelf: 'center',
   },
   createAccountTitle: {
